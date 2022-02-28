d404c1d4229461fde899ab0a5a9f50742570fa0d
gvilkson <gvilkson@gmail.com>
2022-02-28 16:32:00 -0400

    Upgrade

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import os
 import time as tm
 import pandas
 import platform
-
+from threading import Thread as Th
 from termcolor import colored
 
 # Modules ---------
 from tools.content import Server as S
+from tools.content import ServerLocal
 from dialog.dialog import Dialog as msg
 
-#exeple--------------------------------------
+# exeple--------------------------------------
 """
 print(colored('Error Test!!!', 'red'))
 print(colored('Warning Test!!!', 'yellow'))
@@ -19,96 +20,106 @@ print(colored('Success Test!!!', 'green'))
 """
 
 # Funções de classes ---
+
+
 def entry():
     data = input('#'+colored('|set|', 'yellow')+'#>'+' ')
     return data
 
+
 def response(data):
     return data
 
+
 class Main(object):
-    
-	_USUARIO = None
-	_SESSION_NAME = None
- 
-	_DEBUG = False
- 
-	def __init__(self):
-		pass
 
-	def commands(self, commit):
-		if commit.lower() == 'server':
-			server = S()
-			server.build('0.0.0.0', 5003)
-			server.run()
-   
-		elif commit.lower() == 'exit':
-			os.system('clear')
-			print('Fechando aplicação!')
-			return exit()
+    _USUARIO = None
+    _SESSION_NAME = None
 
-		elif commit.lower() == 'login':
-			return print('Em desenvolvimento....')
+    _DEBUG = False
 
-		elif commit.lower() == 'help':
-			return print('Em desenvolvimento....')
+    def __init__(self):
+        pass
 
-		elif commit.lower() == 'gitconfig':
-			return print('Em desenvolvimento....')
+    def commands(self, commit):
+        if commit.lower() == 'server':
+            server = S()
+            server.build('0.0.0.0', 5003)
+            server.run()
 
-		elif commit.lower() == 'herokuconfig':
-			return print('Em desenvolvimento....')
+        elif commit.lower() == 'exit':
+            os.system('clear')
+            print('Fechando aplicação!')
+            return exit()
 
-		elif commit.lower() == 'server web local':
+        elif commit.lower() == 'login':
+            return print('Em desenvolvimento....')
 
-	def methodos(self, argv):
-		triggers = {
-			'commands': [
-       
-				'server',
-				'help',
-				'exit',
-				'login',
-				'gitconfig',
-				'herokuconfig',
-				'server web local',
-    
+        elif commit.lower() == 'help':
+            return print('Em desenvolvimento....')
+
+        elif commit.lower() == 'gitconfig':
+            return print('Em desenvolvimento....')
+
+        elif commit.lower() == 'herokuconfig':
+            return print('Em desenvolvimento....')
+
+        elif commit.lower() == 'server web local':
+            obj = ServerLocal('teste')
+            thred = Th(target=obj.start_server)
+            thred.start()
+
+    def methodos(self, argv):
+        triggers = {
+            'commands': [
+
+                'server',
+                'help',
+                'exit',
+                'login',
+                'gitconfig',
+                'herokuconfig',
+                'server web local',
+
             ],
-		}
+        }
 
-		# Avaliando comandos enviados ...
-		for cmd in triggers['commands']:
-			if argv == cmd:
-				return self.commands(commit=cmd)
+        # Avaliando comandos enviados ...
+        for cmd in triggers['commands']:
+            if argv == cmd:
+                return self.commands(commit=cmd)
 
-			if not argv in triggers['commands']:
-				return print(colored('[-]', 'red'), colored('Comando não encontrado...', 'red'))
+            if not argv in triggers['commands']:
+                return print(colored('[-]', 'red'), colored('Comando não encontrado...', 'red'))
 
 # Processos de lógica para manipulação ...
-def builder():
-		print('Sistema Operacional', colored(' Windows', 'green'))
-		sessao = Main()
 
-		while True:
-			data = entry()
-			if data:
-				if data:
-					sessao.methodos(data)
+
+def builder():
+    print('Sistema Operacional', colored(' Windows', 'green'))
+    sessao = Main()
+
+    while True:
+        data = entry()
+        if data:
+            if data:
+                sessao.methodos(data)
+
 
 def main():
-    
-	# Verificação do sistema...
-	so = platform.system()
-	if so == 'Linux':
-		builder()
-  
-	elif so == 'Windows':
-		print('Sistema Operacional', colored(' Windows', 'green'))
-		print(colored('Windows not suported', 'red'))
-  
-	else:
-		print('Sistema Operacional', colored(' Não Identificado', 'red'))
-		print(colored('Not suported', 'red'))
+
+    # Verificação do sistema...
+    so = platform.system()
+    if so == 'Linux':
+        builder()
+
+    elif so == 'Windows':
+        print('Sistema Operacional', colored(' Windows', 'green'))
+        print(colored('Windows not suported', 'red'))
+
+    else:
+        print('Sistema Operacional', colored(' Não Identificado', 'red'))
+        print(colored('Not suported', 'red'))
 
 
 if __name__ == '__main__':
--- tools/content.py
+++ tools/content.py
@@ -1,122 +1,141 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import socket
 import os
 import subprocess
 import sys
 import tqdm
-
+import subprocess
 from termcolor import colored as c
 
 class Server(object):
 
-	client_socket = None
-	client_address = None
-	cwb = None
+    client_socket = None
+    client_address = None
+    cwb = None
 
-	def __init__(self):
-		self.SERVER_HOST = "0.0.0.0"
-		self.SERVER_PORT = 5003
-		self.BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
-		# separator string for sending 2 messages in one go
-		self.SEPARATOR = "<sep>"
-		# create a socket object
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self):
+        self.SERVER_HOST = "0.0.0.0"
+        self.SERVER_PORT = 5003
+        self.BUFFER_SIZE = 1024 * 128  # 128KB max size of messages, feel free to increase
+        # separator string for sending 2 messages in one go
+        self.SEPARATOR = "<sep>"
+        # create a socket object
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def bind(self):
-		# bind the socket to all IP addresses of this host
-		self.s.bind((self.SERVER_HOST, self.SERVER_PORT))
+    def bind(self):
+        # bind the socket to all IP addresses of this host
+        self.s.bind((self.SERVER_HOST, self.SERVER_PORT))
 
-	def listen(self):
-		self.s.listen(5)
-		return print(c(f"Listening as {self.SERVER_HOST}:{self.SERVER_PORT} ...", 'green'))
+    def listen(self):
+        self.s.listen(5)
+        return print(c(f"Listening as {self.SERVER_HOST}:{self.SERVER_PORT} ...", 'green'))
 
-	def accept(self):
-		# accept any connections attempted
-		self.client_socket, self.client_address = self.s.accept()
-		print(f"{self.client_address[0]}:{self.client_address[1]} Connected!")
+    def accept(self):
+        # accept any connections attempted
+        self.client_socket, self.client_address = self.s.accept()
+        print(f"{self.client_address[0]}:{self.client_address[1]} Connected!")
 
-	def check(self):
-		# receiving the current working directory of the client
-		self.cwd = self.client_socket.recv(self.BUFFER_SIZE).decode()
-		print(c("[+] Current working directory:", 'green'), self.cwd)
+    def check(self):
+        # receiving the current working directory of the client
+        self.cwd = self.client_socket.recv(self.BUFFER_SIZE).decode()
+        print(c("[+] Current working directory:", 'green'), self.cwd)
 
-	def build(self, host, port):
-		self.SERVER_HOST = host
-		self.SERVER_PORT = port
+    def build(self, host, port):
+        self.SERVER_HOST = host
+        self.SERVER_PORT = port
 
-	def run(self):
-		self.bind()
-		self.listen()
-		self.accept()
-		self.check()
+    def run(self):
+        self.bind()
+        self.listen()
+        self.accept()
+        self.check()
 
-		while True:
-			try:
-				# get the command from prompt
-				command = input(f"{self.cwd} $> ")
-				if not command.strip():
-					# empty command
-					continue
+        while True:
+            try:
+                # get the command from prompt
+                command = input(f"{self.cwd} $> ")
+                if not command.strip():
+                    # empty command
+                    continue
 
-				# send the command to the client
-				self.client_socket.send(command.encode())
-				if command.lower() == "exit":
-					# if the command is exit, just break out of the loop
-					break
-				# retrieve command results
-				output = self.client_socket.recv(self.BUFFER_SIZE).decode()
-				# split command output and current directory
-				results, cwd = output.split(self.SEPARATOR)
-				# print output
-				print(results)
-			except:
-				pass
+                # send the command to the client
+                self.client_socket.send(command.encode())
+                if command.lower() == "exit":
+                    # if the command is exit, just break out of the loop
+                    break
+                # retrieve command results
+                output = self.client_socket.recv(self.BUFFER_SIZE).decode()
+                # split command output and current directory
+                results, cwd = output.split(self.SEPARATOR)
+                # print output
+                print(results)
+            except:
+                pass
+
+
 class ServerFileTransfer(Server):
-    
-	# create the server socket
-	# TCP socket
-	s = socket.socket()
 
-	# Progress the size
-	progress = None
+    # create the server socket
+    # TCP socket
+    s = socket.socket()
 
-	def __init__(self):
-		# device's IP address
-		self.SERVER_HOST = "0.0.0.0"
-		self.SERVER_PORT = 5001
-		# receive 4096 bytes each time
-		self.BUFFER_SIZE = 4096
-		self.SEPARATOR = "<SEPARATOR>"
-		
-	def receive_file_size(self, file, size):
-		# receive the file infos
-		# receive using client socket, not server socket
-		received = self.client_socket.recv(self.BUFFER_SIZE).decode()
+    # Progress the size
+    progress = None
 
-		filename, filesize = received.split(self.SEPARATOR)
-		# remove absolute path if there is
-		filename = os.path.basename(filename)
-		# convert to integer
-		filesize = int(filesize)
+    def __init__(self):
+        # device's IP address
+        self.SERVER_HOST = "0.0.0.0"
+        self.SERVER_PORT = 5001
+        # receive 4096 bytes each time
+        self.BUFFER_SIZE = 4096
+        self.SEPARATOR = "<SEPARATOR>"
 
-		# start receiving the gile from the socket
-		# and writing to the file stream
-		self.progress = tqdm.tqdm(range(filesize))
-		with open(filename, "wb") as f:
-			while True:
-				# read 1024 bytes from the socket (receive)
-				bytes_read = self.client_socket.recv(self.BUFFER_SIZE)
-				if not bytes_read:
-					# nothing is received
-					# file transmitting is done
-					break
-				# write to the file the bytes we just received
-				f.write(bytes_read)
-				# update the progress bar
-				self.progress.update(len(bytes_read))
+    def receive_file_size(self, file, size):
+        # receive the file infos
+        # receive using client socket, not server socket
+        received = self.client_socket.recv(self.BUFFER_SIZE).decode()
 
-				# close the client socket
-				self.client_socket.close()
-				# close the server socket
-				self.s.close()
+        filename, filesize = received.split(self.SEPARATOR)
+        # remove absolute path if there is
+        filename = os.path.basename(filename)
+        # convert to integer
+        filesize = int(filesize)
+
+        # start receiving the gile from the socket
+        # and writing to the file stream
+        self.progress = tqdm.tqdm(range(filesize))
+        with open(filename, "wb") as f:
+            while True:
+                # read 1024 bytes from the socket (receive)
+                bytes_read = self.client_socket.recv(self.BUFFER_SIZE)
+                if not bytes_read:
+                    # nothing is received
+                    # file transmitting is done
+                    break
+                # write to the file the bytes we just received
+                f.write(bytes_read)
+                # update the progress bar
+                self.progress.update(len(bytes_read))
+
+                # close the client socket
+                self.client_socket.close()
+                # close the server socket
+                self.s.close()
+
+
+class ServerLocal(object):
+    CURRENT_SERVER = None
+
+    def __init__(self, server):
+        if os.path.isdir('backend_web'):
+            return print('Estrutura existente no diretorio atual')
+        else:
+            os.system('clear')
+            print('Não exite estrutura!')
+            print('Criando estrutura aguarde ...')
+
+            os.system('django-admin startproject backend_web')
+
+    def start_server(self):
+        os.system('python backend_web/manage.py runserver')
